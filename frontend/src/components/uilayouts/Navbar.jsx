f81e511f911a117ed6bb89da9afb4f2564ed660e
pranav01102000 <prkela26@gmail.com>
2025-06-04 19:42:20 +0530

    navbar error solved

--- frontend/src/components/uilayouts/Navbar.jsx
+++ frontend/src/components/uilayouts/Navbar.jsx
@@ -131,10 +131,9 @@ const Navbar = () => {
 
       {/* Mobile Menu */}
       {open && (
-        <div
-          className={`${
+        <div className={`${
             open ? "flex" : "hidden"
-          } absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}
+          } absolute top-full left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden z-50`}
         >
           <NavLink to="/" onClick={() => setOpen(false)} className="block">
             Home
